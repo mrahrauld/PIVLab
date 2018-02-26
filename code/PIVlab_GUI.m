441458551b2dd3c0326fd4b3419ca7d2c930116d
Harold Somers <somershar@gmail.com>
2018-02-26 17:33:52 +0100

    Revert "commit"
    This reverts commit f368b57cff6f9451ea64a823b10b06225a196d9b.

--- code/PIVlab_GUI.m
+++ code/PIVlab_GUI.m
@@ -6639,11 +6639,22 @@ function pushbutton90_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton90 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-    uiopen('cameraParams')
-    filepaths =retr('filepath');
+    filepaths = retr('filepath');
+    cameraParams = retr('cameraParams');
     for i=1:length(filepaths)
         I = imread(filepaths{i});
         [J,newOrigin] = undistortImage(I,cameraParams,'OutputView','full');
         imwrite(J, filepaths{i})
     end
         
+
+
+% --- Executes on button press in pushbutton91.
+function pushbutton91_Callback(hObject, eventdata, handles)
+    uiopen('cameraParams');
+    cameraParams
+    put('cameraParams',cameraParams);
+    
+% hObject    handle to pushbutton91 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)

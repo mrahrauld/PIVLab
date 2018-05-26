0ce67afe7dddf27ccb5463d14eaba510b38b020f
Harold Somers <somershar@gmail.com>
2018-05-26 18:53:23 +0200

    Revert "test"
    This reverts commit c5ae0bd7ab77701c017749203717d7c481de9992.

--- code/PIVlab_GUI.m
+++ code/PIVlab_GUI.m
@@ -7229,7 +7229,6 @@ function stabilize_Callback(hObject, eventdata, handles)
     filepaths =retr('filepath');
 <<<<<<< HEAD
     fixedPoints = retr('fixedPoints');
-    crop_rect = retr('crop_rect')
     fixedFrame = imread(filepaths{1});
     fixed_ref = imref2d(size(fixedFrame));
     h = waitbar(0,'loading, please wait');
@@ -7244,7 +7243,7 @@ function stabilize_Callback(hObject, eventdata, handles)
         t_concord = fitgeotrans(movingPoints,fixedPoints.Location,'projective');
         moving_registered = imwarp(frame,t_concord,'OutputView',fixed_ref);
         imshow(moving_registered);
-        imwrite(imcrop(moving_registered,crop_rect), filepaths{i})
+        imwrite(moving_registered, filepaths{i})
     end
     close(h)
 =======
